5741be4d8a01c51f5dfa8929afe5980a513aacdc
Anton Saukkonen <63663359+antsaukk@users.noreply.github.com>
2021-08-26 10:25:51 +0300

    Assert and AssertEqual are improved with macros. << op overloaded also for a vector.

--- test_framework_model.cpp
+++ test_framework_model.cpp
@@ -1,11 +1,28 @@
+#pragma once
+
 #include <sstream>
-#include <exception>
+#include <stdexcept>
 #include <iostream>
-#include <string>
 #include <map>
 #include <set>
+#include <string>
+#include <vector>
 
 using namespace std;
+
+template <class T>
+ostream& operator << (ostream& os, const vector<T>& s) {
+  os << "{";
+  bool first = true;
+  for (const auto& x : s) {
+    if (!first) {
+      os << ", ";
+    }
+    first = false;
+    os << x;
+  }
+  return os << "}";
+}
 
 template <class T>
 ostream& operator << (ostream& os, const set<T>& s) {
@@ -36,13 +53,13 @@ ostream& operator << (ostream& os, const map<K, V>& m) {
 }
 
 template<class T, class U>
-void AssertEqual(const T& t, const U& u,
-    const string& hint)
-{
-  if (t != u) {
+void AssertEqual(const T& t, const U& u, const string& hint = {}) {
+  if (!(t == u)) {
     ostringstream os;
-    os << "Assertion failed: " << t << " != " << u
-       << " hint: " << hint;
+    os << "Assertion failed: " << t << " != " << u;
+    if (!hint.empty()) {
+       os << " hint: " << hint;
+    }
     throw runtime_error(os.str());
   }
 }
@@ -56,12 +73,15 @@ public:
   template <class TestFunc>
   void RunTest(TestFunc func, const string& test_name) {
     try {
-	  func();
-	  cerr << test_name << " OK" << endl;
-	} catch (runtime_error& e) {
-	  ++fail_count;
-	  cerr << test_name << " fail: " << e.what() << endl;
-	}
+      func();
+      cerr << test_name << " OK" << endl;
+    } catch (exception& e) {
+      ++fail_count;
+      cerr << test_name << " fail: " << e.what() << endl;
+    } catch (...) {
+      ++fail_count;
+      cerr << "Unknown exception caught" << endl;
+    }
   }
 
   ~TestRunner() {
@@ -74,3 +94,20 @@ public:
 private:
   int fail_count = 0;
 };
+
+#define ASSERT_EQUAL(x, y) {            \
+  ostringstream os;                     \
+  os << #x << " != " << #y << ", "      \
+    << __FILE__ << ":" << __LINE__;     \
+  AssertEqual(x, y, os.str());          \
+}
+
+#define ASSERT(x) {                     \
+  ostringstream os;                     \
+  os << #x << " is false, "             \
+    << __FILE__ << ":" << __LINE__;     \
+  Assert(x, os.str());                  \
+}
+
+#define RUN_TEST(tr, func) \
+  tr.RunTest(func, #func)
